dcaae3c1e61faaf363d3ab3707db17c8e8a7dcc6
nothing-c <55337124+nothing-c@users.noreply.github.com>
2022-09-11 20:41:59 -0500

    Update cookiemonster.c

--- cookiemonster.c
+++ cookiemonster.c
@@ -1,13 +1,14 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void main(){
 	initscr();
 	here:
 	addstr("GIVE ME COOKIES");
 	char* in = scanf("s");
-	if (*in == "cookies"){
+	if (strcmp(in, "cookies") == 0){
 		addstr("Thank you :3");
 		endwin();
 		exit();
